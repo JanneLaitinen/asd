59d7b6c2ffe2722845289862121a4bb8ed781b76
Laitinen <Janne.Laitinen@seamk.fi>
2018-02-13 12:36:00 +0200

    lisätty tiedostoja

---
+++ MQTTClientTest1/MQTTClientTest1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// including the M2Mqtt Library
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace MQTTClientTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("start");
+            // Create Client instance
+            MqttClient myClient = new MqttClient("192.168.28.129");
+
+            // Register to message received
+            myClient.MqttMsgPublishReceived += client_recievedMessage;
+
+            string clientId = Guid.NewGuid().ToString();
+            myClient.Connect(clientId);
+
+            // Subscribe to topic
+            myClient.Subscribe(new String[] { "measurements/#" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+            Console.ReadLine();
+        }
+
+
+        static void client_recievedMessage(object sender, MqttMsgPublishEventArgs e)
+        {
+            // Handle message received
+            var message = Encoding.Default.GetString(e.Message);
+            Console.WriteLine("Message received: " + message);
+        }
+    }
+}
---
+++ MQTTpublisher/MQTTpublisher/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace MQTTpublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("start publishing");
+            // Create Client instance
+            MqttClient myClient = new MqttClient("192.168.28.130");
+
+            string clientId = Guid.NewGuid().ToString();
+            myClient.Connect(clientId);
+
+            string strValue = Convert.ToString("hei hei");
+            myClient.Publish("test", Encoding.UTF8.GetBytes(strValue));
+        }
+
+    }
+}
